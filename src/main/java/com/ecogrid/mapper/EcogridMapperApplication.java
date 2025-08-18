ab02f5006f207892d125a652c610cdb38c152420
Luis <luishenriqueds01@gmail.com>
2025-08-18 20:53:51 -0300

    Commit inicial local

---
+++ src/main/java/com/ecogrid/mapper/EcogridMapperApplication.java
@@ -0,0 +1,13 @@
+package com.ecogrid.mapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcogridMapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcogridMapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecogrid/mapper/EcogridMapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecogrid.mapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcogridMapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
